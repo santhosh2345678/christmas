797071e07210ef8a620416ff4da348508754bf39
santhosh2345678 <143503610+santhosh2345678@users.noreply.github.com>
2023-12-27 10:22:01 +0530

    added all

---
+++ script/script.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($){
+   $('#menu').click(function(){
+    $('.nav').removeClass('active');
+   });
+   $('#close').click(function(){
+    $('.nav').addClass('active');
+   });
+   $('#icon').click(function(){
+    $('body').toggleClass('active')
+   });
+   $('#mobile-icon').click(function(){
+    $('body').toggleClass('active')
+   });
+});
